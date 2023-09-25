5a90da60524844b7283a9160a7edb49011dd5b41
LucWehrenss <lj.wehrens@student.maastrichtuniversity.nl>
2023-09-25 14:59:04 +0200

    added a python script to parse the text files

---
+++ parseTextFiles.py
@@ -0,0 +1,21 @@
+def parse_text_file(file_to_parse):
+    entities = []
+    current_entity = {}
+    with open(file_to_parse, 'r') as file:
+        for line in file:
+            line.strip()
+            if line.startswith('---'):
+                if current_entity:
+                    entities.append(current_entity)
+                current_entity = {}
+            elif '=' in line:
+                key, value = line.split('=')
+                current_entity[key.strip('"')] = value.strip('"')
+    if(current_entity):
+        entities.append(current_entity)
+    return entities
+
+#parse the text_file
+path_of_file_to_parse = "generated_entities.txt"
+parsed_entities = parse_text_file(path_of_file_to_parse)
+print(parsed_entities)
