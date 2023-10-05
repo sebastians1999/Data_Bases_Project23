57ec44b100dfe2ac72373ad77d2671547502b605
LucWehrenss <lj.wehrens@student.maastrichtuniversity.nl>
2023-10-05 16:12:21 +0200

    added npc table and character
    character table doesn't work well for some reason, can't figure out why
    i think it has something to do with the foreign relations, but i tried to get it to work with just id and name and that also didn't work
    im out of ideas

--- addDataToSQLTables.py
+++ addDataToSQLTables.py
@@ -25,20 +25,38 @@ parsed_entities = parseGeneratedEntities.parse_generated_entities(path_of_file_t
 #simple method to clear a table
 def clear_table(table_to_clear):
     query = "DELETE FROM " + table_to_clear
+    print(query)
     cursor.execute(query)
 
 #clearing all tables before entering info again
+# clear_table("character")
 clear_table("item")
-clear_table("event")
 clear_table("enemy")
+clear_table("event")
+clear_table("npc")
 clear_table("team")
 
 for entity in parsed_entities:
+    #Character table is being a big piece of shit poopy idiot idk try to see if you can fix it
+    # # Character
+    # if 'Character' == entity['category']:
+    #     # print(entity)
+    #     query = "INSERT INTO character (id, name, race, class) VALUES (%s, %s, %s, %s)"
+    #     values = (entity['id'], entity['first_name'] + " " + entity['firstname'], entity['race'], entity['class'])
+    #     cursor.execute(query, values)
+
     # Items
     if 'Item' == entity['category']:
         # print(entity)
         query = "INSERT INTO item (id, name) VALUES (%s, %s)"
         values = (entity['id'], entity['item_name'])
+        cursor.execute(query, values)
+        
+    # Enemy
+    if 'Enemy' == entity['category']:
+        # print(entity)
+        query = "INSERT INTO enemy (id, name, type) VALUES (%s, %s, %s)"
+        values = (entity['id'], entity['enemy_name'], entity['enemy_type'])
         cursor.execute(query, values)
 
     # Events
@@ -47,12 +65,12 @@ for entity in parsed_entities:
         query = "INSERT INTO event (id, name) VALUES (%s, %s)"
         values = (entity['id'], entity['event_name'])
         cursor.execute(query, values)
-        
-    # Enemy
-    if 'Enemy' == entity['category']:
+
+    # NPC
+    if 'NPC' == entity['category']:
         # print(entity)
-        query = "INSERT INTO enemy (id, name, type) VALUES (%s, %s, %s)"
-        values = (entity['id'], entity['enemy_name'], entity['enemy_type'])
+        query = "INSERT INTO npc (id, name, role) VALUES (%s, %s, %s)"
+        values = (entity['id'], entity['first_name'] + " " + entity['last_name'], entity['npc_type'])
         cursor.execute(query, values)
 
     # Team
