c41a57500feec156d6b0fc1f2615ada258148e30
LucWehrenss <lj.wehrens@student.maastrichtuniversity.nl>
2023-10-17 17:32:47 +0200

    big changes
    added function to add data to different tables

--- addDataToSQLTables.py
+++ addDataToSQLTables.py
@@ -1,4 +1,5 @@
 import parseGeneratedEntities
+import parseGeneratedEvents
 import os
 import mysql.connector
 
@@ -10,10 +11,10 @@ db_password = os.environ.get('DB_PASSWORD')
 db_database = os.environ.get('DB_DATABASE')
 
 
-db_host = "localhost"
-db_user = "root"
-db_password = "Bordcomputer3"
-db_database = "MysticQuestDBgit"
+# db_host = "localhost"
+# db_user = "root"
+# db_password = "Bordcomputer3"
+# db_database = "MysticQuestDBgit"
 
 conn = mysql.connector.connect(
     host=db_host,
@@ -23,11 +24,15 @@ conn = mysql.connector.connect(
 )
 
 cursor = conn.cursor()
+cursor = conn.cursor(buffered=True)
 
 #parse the text_file
 path_of_file_to_parse = "generated_entities.txt"
 parsed_entities = parseGeneratedEntities.parse_generated_entities(path_of_file_to_parse)
+path_of_file_to_parse = "generated_events.txt"
+parsed_events = parseGeneratedEvents.parse_generated_events(path_of_file_to_parse)
 
+#method to clear a tables foreign key constraints
 def clear_key_constraints(table_to_clear):
     query = "ALTER TABLE " + table_to_clear + " DISABLE KEYS;"
     cursor.execute(query)
@@ -38,24 +43,61 @@ def clear_table(table_to_clear):
     #print(query)
     cursor.execute(query)
 
+#method to reset the auto_increment of a table
+def reset_auto_increment(table_to_reset):
+    query = "ALTER TABLE " + table_to_reset + " AUTO_INCREMENT = 1"
+    cursor.execute(query)
+
+#method to insert e.g. 'race' from a character into the race table
+#returns the id of the race to insert into the race_id table
+def insert_entity_data(table_name, table_to_insert_to, entity, conn, cursor):
+    exists_query = "SELECT IF(EXISTS(SELECT * FROM {} WHERE name LIKE %s), 'YES', 'NO')".format(table_name)
+    entity_name = entity[table_name.lower()]  # assumes the entity name is a column with lowercase name
+
+    cursor.execute(exists_query, (entity_name,))
+    result = cursor.fetchone()
+
+    if result is not None and result[0] == 'YES':
+        # entity exists
+        pass
+    else:
+        # entity doesnt exist, insert it here
+        insert_query = "INSERT INTO {} (name) VALUES (%s)".format(table_name)
+        cursor.execute(insert_query, (entity_name,))
+        conn.commit()
+
+    # get the id of the entity
+    id_query = "SELECT id FROM {} WHERE name LIKE %s".format(table_name)
+    cursor.execute(id_query, (entity_name,))
+    entity_id = cursor.fetchone()
+
+    if entity_id is not None:
+        entity_id = entity_id[0]
+        return entity_id
+
 #clearing all tables before entering info again
+# clear_key_constraints("characters")
 clear_table("characters")
 clear_table("item")
 clear_table("enemy")
 clear_table("event")
 clear_table("npc")
 clear_table("team")
+clear_table("race")
+reset_auto_increment("race")
+reset_auto_increment("class")
 
 for entity in parsed_entities:
-    #Character table is being a big piece of shit poopy idiot idk try to see if you can fix it
-    #NEVERMIND :DDDD
     # Character
     if 'Character' == entity['category']:
         # print(entity)
-        query = "INSERT INTO characters (id, name, race, class) VALUES (%s, %s, %s, %s)"
-        values = (entity['id'], entity['first_name'] + " " + entity['firstname'], entity['race'], entity['class'])
+        race_id = insert_entity_data('race', 'Character', entity, conn, cursor)
+        class_id = insert_entity_data('class', 'Character', entity, conn, cursor)
+
+        query = "INSERT INTO characters (id, name, race_id, class_id) VALUES (%s, %s, %s, %s)"
+        values = (entity['id'], entity['first_name'] + " " + entity['firstname'], race_id, class_id)
         cursor.execute(query, values)
-        
+
     # Enemy
     if 'Enemy' == entity['category']:
         # print(entity)
@@ -92,9 +134,16 @@ for entity in parsed_entities:
     # Team
     if 'Team' == entity['category']:
         # print(entity)
-        query = "INSERT INTO team (id, name, number_of_players) VALUES (%s, %s, %s)"
-        values = (entity['id'], entity['team_name'], entity['n_members'])
+        kingdom_id = insert_entity_data('kingdom', 'Team', entity, conn, cursor)
+        
+        query = "INSERT INTO team (id, name, number_of_players, kingdom_id) VALUES (%s, %s, %s, %s)"
+        values = (entity['id'], entity['team_name'], entity['n_members'], kingdom_id)
         cursor.execute(query, values)
 
+# for event in parsed_events[:10]:
+#     print(event)
+
+
 conn.commit()
+cursor.close()
 conn.close()
